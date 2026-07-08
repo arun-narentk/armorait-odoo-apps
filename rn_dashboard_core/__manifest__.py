025c4558640e99ff82b1b9c6745c97eda999b938
Arun Keshav <arun.keshav@trabacus.com>
2026-07-09 01:04:54 +0530

    Add live_test_url to Apps Store manifest

--- rn_dashboard_core/__manifest__.py
+++ rn_dashboard_core/__manifest__.py
@@ -23,6 +23,7 @@ depend on this core instead of duplicating chart, filter, or alert logic.
     'license': 'OPL-1',
     'currency': 'USD',
     'price': 49.99,
+    'live_test_url': 'https://www.armorait.com',
     'sequence': 40,
     'depends': [
         'base',
